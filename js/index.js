38b65843b57ebf1e4d0598294d686f88912185ba
XTDPotato <2634343102@qq.com>
2024-09-23 23:23:27 +0800

    更新页面

--- js/index.js
+++ js/index.js
@@ -2,9 +2,14 @@
 "use strict"; // 添加严格模式指令
 let navigationDrawer = document.querySelector(".main-drawer"); // 使用 let 声明可变对象
 let openDrawerButton = document.getElementById("fab_menu");
+let main_drawer = document.querySelector(".main-drawer");
 let closeDrawerButton = navigationDrawer.querySelector("mdui-button");
+let drawer_btn1 = navigationDrawer.querySelector(".drawer_btn1");
+let drawer_btn2 = navigationDrawer.querySelector(".drawer_btn2");
+let drawer_btn3 = navigationDrawer.querySelector(".drawer_btn3");
+
 let main_tabs = document.getElementById("main_tabs");
-let main_nv_home = document.getElementById("rail_item_home");
+let main_nv_saying = document.getElementById("rail_item_saying");
 let main_nv_resources = document.getElementById("rail_item_resources");
 let main_nv_notes = document.getElementById("rail_item_notes");
 let main_nv_courses = document.getElementById("rail_item_courses");
@@ -19,6 +24,25 @@ let note_frame = document.getElementById("note_frame");
 let notes_item_frame = document.getElementById("notes_item_frame");
 let resources_item_frame = document.getElementById("resources_item_frame");
 let main_fab_click_change = 0;
+
+
+
+
+  
+//打开抽屉并关闭
+/*navigationDrawer.open = true;
+setTimeout(function(){
+	navigationDrawer.open = false;
+	setTimeout(function(){
+	navigationDrawer.open = true;
+	setTimeout(function(){
+	navigationDrawer.open = false;
+	
+}, 500);
+}, 250);
+}, 500);
+*/
+
 
 
 
@@ -71,19 +95,26 @@ main_fab.icon = "refresh--outlined"
   
   //点击菜单的打开抽屉点击事件
   openDrawerButton.addEventListener("click", ()=>{
-  navigationDrawer.open = true;
 
+  if(main_drawer.open == false){
+	  navigationDrawer.open = true;
+  }else{
+	  navigationDrawer.open = false;
+  }
+  
 });
 
-  //抽屉里面的关闭抽屉按钮点击事件
-  closeDrawerButton.addEventListener("click", () =>{
+  //抽屉按钮点击事件
+  drawer_btn1.addEventListener("click", () =>{
   navigationDrawer.open = false;
   
   });
   
- //标签页主页点击事件
- main_nv_home.addEventListener("click", ()=>{
-  main_tabs.value = "home";
+  
+  
+ //标签页话说点击事件
+ main_nv_saying.addEventListener("click", ()=>{
+  main_tabs.value = "saying";
   main_fab_click_change = 0;
   main_fab.disabled = false;
   main_fab.icon = "refresh--outlined";
